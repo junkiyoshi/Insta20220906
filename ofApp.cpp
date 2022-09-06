3b472225b11300226cc062165e7132c2e6fb6901
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-09-06 19:42:05 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,125 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(30);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(255);
+	ofSetLineWidth(2);
+	ofEnableDepthTest();
+
+	auto ico_sphere = ofIcoSpherePrimitive(1, 5);
+	this->base_location_list = ico_sphere.getMesh().getVertices();
+
+	this->number_of_sphere = 3000;
+	while (this->sphere_list.size() < this->number_of_sphere) {
+
+		int index = ofRandom(this->base_location_list.size());
+		auto tmp_location = this->base_location_list[index];
+		tmp_location = glm::normalize(tmp_location) * ofRandom(0, 100);
+
+		auto radius = this->sphere_list.size() < 110 ? ofRandom(10, 50) : ofRandom(2, 20);
+
+		bool flag = true;
+		for (int i = 0; i < this->sphere_list.size(); i++) {
+
+			if (glm::distance(tmp_location, get<1>(this->sphere_list[i])) < get<2>(this->sphere_list[i]) + radius) {
+
+				flag = false;
+				break;
+			}
+		}
+
+		if (flag) {
+
+			ofColor color;
+			color.setHsb(ofRandom(255), 200, 255);
+
+			auto size = (radius * 2) / sqrt(3);
+
+			this->sphere_list.push_back(make_tuple(color, tmp_location, size));
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateY(ofGetFrameNum() * 0.333333333333333);
+
+	for (int i = 0; i < this->sphere_list.size(); i++) {
+
+		auto location = get<1>(this->sphere_list[i]);
+		auto size = get<2>(this->sphere_list[i]);
+
+		ofPushMatrix();
+		ofTranslate(location);
+		ofRotateZ(ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.003), 0, 1, -360, 360));
+		ofRotateY(ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.003), 0, 1, -360, 360));
+		ofRotateX(ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.003), 0, 1, -360, 360));
+
+		ofRotateZ(ofRandom(360));
+		ofRotateY(ofRandom(360));
+		ofRotateX(ofRandom(360));
+
+		this->draw_arrow(glm::vec2(), glm::vec2(1, 1), size);
+
+		ofPopMatrix();
+	}
+
+	this->cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw_arrow(glm::vec2 location, glm::vec2 next_location, float size) {
+
+	auto angle = std::atan2(next_location.y - location.y, next_location.x - location.x);
+
+	ofSetColor(0);
+	ofFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.8 * cos(angle), size * 0.8 * sin(angle)) + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) + location);
+	ofEndShape(true);
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 + location);
+	ofEndShape(true);
+
+	ofSetColor(255);
+	ofNoFill();
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) + location);
+	ofVertex(glm::vec2(size * 0.8 * cos(angle), size * 0.8 * sin(angle)) + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 + location);
+	ofEndShape();
+
+	ofBeginShape();
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle + PI * 0.5), size * 0.5 * sin(angle + PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 - glm::vec2(size * cos(angle), size * sin(angle)) * 0.5 + location);
+	ofVertex(glm::vec2(size * 0.5 * cos(angle - PI * 0.5), size * 0.5 * sin(angle - PI * 0.5)) * 0.25 + location);
+	ofEndShape();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
